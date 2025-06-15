bc7215c1f79315ca25a23a6f3257264c6ce007c0
Mayank Pandey <codebymayank@gmail.com>
2025-06-15 17:54:36 +0530

    Replaced bcrypt with bcryptjs for Render deployment

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
 import User from '../models/user.js'
